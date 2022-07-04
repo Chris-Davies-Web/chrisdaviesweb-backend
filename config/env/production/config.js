16a41290c691079a2abcd079fe32d6dbe692e07b
chrisdaviesweb <chris@chrisdaviesweb.com>
2022-07-04 18:36:48 +0100

    db update

--- config/env/production/config.js
+++ config/env/production/config.js
@@ -1,19 +1,19 @@
-// path: ./config/env/production/database.js
-
 module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'), 
-        port: env.int('DATABASE_PORT'), 
-        database: env('DATABASE_NAME'), 
-        user: env('DATABASE_USERNAME'), 
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          ca: env('DATABASE_CA')
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
+        },
+        options: {
+          ssl: false,
         },
       },
-      debug: false,
     },
   });
-  
